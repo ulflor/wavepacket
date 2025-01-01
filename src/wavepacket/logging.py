35cf3ff22e7b62ec54fed32786e834d040f822e8
Ulf Lorenz <ulf@wppy.org>
2025-01-01 21:30:05 +0000

    Logging output improvements
    - fix code
    - added a precision parameter to remove clutter

--- src/wavepacket/logging.py
+++ src/wavepacket/logging.py
@@ -4,14 +4,14 @@ from .grid import State, trace
 from .operator import Potential1D, expectation_value
 
 
-def log(state: State, t: float) -> None:
-    print(f"t = {t},     trace = {trace(state)}\n")
+def log(t: float, state: State, precision: int = 6) -> None:
+    print(f"\n\nt = {t:.{precision}},     trace = {trace(state):.{precision}}\n")
 
     for index, dof in enumerate(state.grid.dofs):
         x = Potential1D(state.grid, 0, lambda dvr_grid: dvr_grid)
         x2 = Potential1D(state.grid, 0, lambda dvr_grid: dvr_grid ** 2)
 
-        x_expect = (expectation_value(x, state)).real
+        x_expect = expectation_value(x, state).real
         x_expect2 = expectation_value(x2, state).real
 
-        print(f"<x_{index}> = {x_expect}  =/- {math.sqrt(x_expect ** 2 - x_expect2)}")
+        print(f"<x_{index}> = {x_expect:.{precision}}  =/- {math.sqrt(x_expect2 - x_expect ** 2):.{precision}}")
