e4c81185c191f70420b6e190ac72847d51f0b2b2
Ulf Lorenz <ulf@wppy.org>
2025-08-29 15:36:31 +0000

    #20 Extended projection operator to project onto a subspace
    You can now define multiple functions, and the operator projects onto
    the subspace spanned by these functions. Might be a useful extension.

--- src/wavepacket/operator/misc_operators.py
+++ src/wavepacket/operator/misc_operators.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 from typing import Sequence
 
@@ -6,51 +5,72 @@ import wavepacket as wp
 import wavepacket.typing as wpt
 
 from .operatorbase import OperatorBase
-from ..grid import State, trace
+from ..grid import State, trace, orthonormalize
 
 
 class Projection(OperatorBase):
     """
-    Projects onto a given wave function.
+    Projects onto a subspace defined by one or more basis functions.
 
     This class represents an idempotent projection operator,
-    :math:`\\hat P = |\\psi\\rangle\\langle\\psi|`.
+    :math:`\\hat P = \\sum_i |\\psi_i\\rangle\\langle\\psi_i|`,
+    where the basis functions :math:`\\psi_i` span the subspace on
+    which the operator projects.
+
+    The basis functions need not be orthogonal, they are orthonormalized
+    using :func:`wp.grid.orthonormalize`.
 
     Parameters
     ----------
-    state: wp.grid.State
-      The wave function :math:`\\psi` onto which we project the input.
-      Does not need to be normalized.
+    basis: wp.grid.State | Sequence[wp.grid.State]
+      The basis functions :math:`\\psi_i` that span the subspace onto which the operator projects.
+      Need not be orthogonal, but should be linearly independent.
+      In case of one basis function, it can be supplied directly without a list.
 
     Raises
     ------
     wp.BadStateException
-      Thrown if the state is not a wave function or has norm zero.
+        Thrown if any basis function is not a wave function or has norm zero.
+
+    wp.InvalidValueError
+        Thrown if no basis functions are supplied.
     """
 
-    def __init__(self, state: State | Sequence[State]):
-        if not state.is_wave_function():
-            raise wp.BadStateError("Can only project onto a wave function.")
+    def __init__(self, basis: State | Sequence[State]):
+        if isinstance(basis, wp.grid.State):
+            basis = [basis]
 
-        if trace(state) == 0:
-            raise wp.BadStateError("Wave function to project on must not have norm zero.")
+        if not basis:
+            raise wp.InvalidValueError("Projection operator requires at least one state to project onto.")
 
-        self._ket = state.data / math.sqrt(trace(state))
-        self._bra = np.conj(self._ket)
-        self._ket_vector = np.ravel(self._ket)
-        self._bra_vector = np.ravel(self._bra)
-        super().__init__(state.grid)
+        for state in basis:
+            if not state.is_wave_function():
+                raise wp.BadStateError("Can only project onto wave functions.")
+
+            if trace(state) == 0:
+                raise wp.BadStateError("Basis functions must not have norm zero.")
+
+        orthonormal_basis = orthonormalize(basis)
+
+        self._ket_nd = np.stack([s.data for s in orthonormal_basis])
+        self._bra_nd = np.conj(self._ket_nd)
+        self._ket_ravelled = np.reshape(self._ket_nd, (len(basis), basis[0].grid.size))
+        self._bra_ravelled = np.conj(self._ket_ravelled)
+        super().__init__(basis[0].grid)
 
     def apply_to_wave_function(self, psi: wpt.ComplexData, t: float) -> wpt.ComplexData:
-        coefficient = (self._bra * psi).sum()
-        return coefficient * self._ket
+        tmp = np.reshape(psi, self._grid.size)
+        coefficients = np.tensordot(self._bra_ravelled, tmp, axes=(1, 0))
+        return np.tensordot(self._ket_nd, coefficients, axes=(0, 0))
 
     def apply_from_left(self, rho: wpt.ComplexData, t: float) -> wpt.ComplexData:
         matrix_form = np.reshape(rho, (self._grid.size, self._grid.size))
-        coefficients = np.tensordot(self._bra_vector, matrix_form, (0, 0))
-        return np.tensordot(self._ket, coefficients, ((), ()))
+        coefficients = np.tensordot(self._bra_ravelled, matrix_form, (1, 0))
+        ket_projection = np.tensordot(self._ket_nd, coefficients, (0, 0))
+        return np.reshape(ket_projection, self._grid.operator_shape)
 
     def apply_from_right(self, rho: wpt.ComplexData, t: float) -> wpt.ComplexData:
         matrix_form = np.reshape(rho, (self._grid.size, self._grid.size))
-        coefficients = np.tensordot(matrix_form, self._ket_vector, (1, 0))
-        return np.tensordot(coefficients, self._bra, ((), ()))
+        coefficients = np.tensordot(matrix_form, self._ket_ravelled, (1, 1))
+        bra_projection = np.tensordot(coefficients, self._bra_nd, (1, 0))
+        return np.reshape(bra_projection, self._grid.operator_shape)
--- test/operator/test_misc_operators.py
+++ test/operator/test_misc_operators.py
@@ -20,7 +20,8 @@ def orthogonal_states(grid: wp.grid.Grid) -> Tuple[wp.grid.State, wp.grid.State]
     return s1 / math.sqrt(wp.grid.trace(s1)), s2 / math.sqrt(wp.grid.trace(s2))
 
 
-def test_projection_requires_normalizable_wave_function(grid_1d):
+def test_invalid_projection_inputs(grid_1d):
+    good_state = wp.testing.random_state(grid_1d, 42)
     bad_state = wp.builder.unit_density(grid_1d)
     zero_state = wp.builder.zero_wave_function(grid_1d)
 
@@ -29,6 +30,15 @@ def test_projection_requires_normalizable_wave_function(grid_1d):
 
     with pytest.raises(wp.BadStateError):
         wp.operator.Projection(zero_state)
+
+    with pytest.raises(wp.InvalidValueError):
+        wp.operator.Projection([])
+
+    with pytest.raises(wp.BadStateError):
+        wp.operator.Projection([good_state, bad_state])
+
+    with pytest.raises(wp.BadStateError):
+        wp.operator.Projection([good_state, zero_state])
 
 
 def test_project_wave_function(grid_2d):
@@ -64,3 +74,39 @@ def test_project_density_operator(grid_1d):
     result_right = projection.apply_from_right(rho.data, 0.0)
     expected_right = wp.builder.direct_product(psi, projected).data
     assert_allclose(result_right, expected_right, rtol=0, atol=1e-12)
+
+
+def test_project_states_onto_subspace(grid_2d):
+    states = [wp.testing.random_state(grid_2d, seed) for seed in range(4)]
+    states = wp.grid.orthonormalize(states)
+
+    subspace = states[1:]
+    projection = wp.operator.Projection(subspace)
+
+    projected = wp.builder.zero_wave_function(grid_2d)
+    for s in subspace:
+        projected = projected + 1.3 * s
+    psi = 5.0 * states[0] + projected
+    rho = wp.builder.pure_density(psi)
+
+    got_psi = projection.apply(psi)
+    assert_close(got_psi, projected, 1e-12)
+
+    expected_from_left = wp.builder.direct_product(projected, psi).data
+    got_from_left = projection.apply_from_left(rho.data, 0.0)
+    assert_allclose(got_from_left, expected_from_left, rtol=0, atol=1e-12)
+
+    expected_from_right = wp.builder.direct_product(psi, projected).data
+    got_from_right = projection.apply_from_right(rho.data, 0.0)
+    assert_allclose(got_from_right, expected_from_right, rtol=0, atol=1e-12)
+
+
+def test_non_orthogonal_states(grid_1d):
+    basis = [wp.testing.random_state(grid_1d, seed) for seed in range(4)]
+    orthogonal_basis = wp.grid.orthonormalize(basis)
+
+    projection1 = wp.operator.Projection(basis)
+    projection2 = wp.operator.Projection(orthogonal_basis)
+    input = wp.testing.random_state(grid_1d, 10)
+
+    assert_close(projection1.apply(input), projection2.apply(input), 1e-12)
