ea7ebc326ad3902aa2459ddeb39ad33f1d0222b4
Ulf Lorenz <ulf@wppy.org>
2025-09-05 21:06:18 +0000

    Updated the version number

--- doc/conf.py
+++ doc/conf.py
@@ -9,7 +9,7 @@
 project = 'Wavepacket'
 copyright = '2025, Ulf Lorenz'
 author = 'Ulf Lorenz'
-release = '0.1'
+release = '0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
