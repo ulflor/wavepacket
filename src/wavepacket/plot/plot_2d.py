bf7c1add294895120dd7c9e590e4b729cb410759
Ulf Lorenz <ulf@wppy.org>
2026-02-27 14:07:16 +0000

    #37 Added marginals to the contour plot
    The (now square) contour plot gets two additional axes to the
    right and bottom that show the reduced densities in x / y. This needed
    heavy tweaking and going beyond the Matplotlib standard layouts. In
    particular, the three axes are aligned manually, and the figure size is
    set explicitly.

--- src/wavepacket/plot/plot_2d.py
+++ src/wavepacket/plot/plot_2d.py
@@ -10,7 +10,13 @@ class ContourPlot2D:
         assert len(state.grid.dofs) == 2
         assert potential is None or potential.grid == state.grid
 
-        self.figure, self._axes = plt.subplots()
+        # Create a square main plot and the two axes for the reduced densities
+        # This seems to be a case that seems not well-supported by the layout engine,
+        # so we use fixed layouts here
+        self.figure = plt.figure(figsize=(8, 8), layout='none')
+        self._axes = self.figure.add_axes((0.1, 0.3, 0.6, 0.6))
+        self._ax_bottom = self.figure.add_axes((0.1, 0.1, 0.6, 0.2), sharex=self._axes)
+        self._ax_right = self.figure.add_axes((0.7, 0.3, 0.2, 0.6), sharey=self._axes)
 
         x = state.grid.dofs[0].dvr_points
         y = state.grid.dofs[1].dvr_points
@@ -24,6 +30,9 @@ class ContourPlot2D:
         max_density = density.max()
         self.contours = np.linspace(0, max_density, 15)
 
+        self.max_marginals = (wp.dvr_density(state, 0).max(),
+                              wp.dvr_density(state, 1).max())
+
         if potential is None:
             self._potential = None
         else:
@@ -32,7 +41,8 @@ class ContourPlot2D:
             self.potential_contours = np.linspace(potential_values.min(),
                                                   potential_values.max(), 15)
 
-    def plot(self, state: wp.grid.State, t: float) -> plt.Axes:
+    def plot(self, state: wp.grid.State, t: float) -> None:
+        # Plot the 2D contour plot
         self._axes.clear()
         self._axes.set_xlim(*self.xlim)
         self._axes.set_ylim(*self.ylim)
@@ -51,8 +61,27 @@ class ContourPlot2D:
                            levels=self.contours, colors='b',
                            linewidths=1, linestyles='-')
 
-        self._axes.set_xlabel("x [a.u.]")
-        self._axes.set_ylabel("y [a.u.]")
-        self._axes.set_title(f"t = {t:.4g} a.u.")
+        # Plot the reduced density along x
+        self._ax_bottom.clear()
+        self._ax_bottom.set_ylim(0, self.max_marginals[0])
+        reduced_density_x = wp.dvr_density(state, 0)
+        self._ax_bottom.plot(x, reduced_density_x, 'b-')
 
-        return self._axes
+        # Plot the reduced density along y
+        self._ax_right.clear()
+        self._ax_right.set_xlim(self.max_marginals[1], 0)
+        reduced_density_y = wp.dvr_density(state, 1)
+        self._ax_right.plot(reduced_density_y, y, 'b-')
+
+        # All the styling: Remove superfluous ticks and such
+        self._axes.set_title(f"t = {t:.4g} a.u.")
+        self._axes.xaxis.set_tick_params(labelbottom=False, tickdir='in', top=True)
+        self._axes.yaxis.set_tick_params(labelleft=False, tickdir='in', right=True)
+
+        self._ax_bottom.set_yticks([])
+        self._ax_bottom.set_xlabel('x_0 (a.u.)')
+
+        self._ax_right.set_xticks([])
+        self._ax_right.yaxis.set_tick_params(labelleft=False, labelright=True, left=False, right=True)
+        self._ax_right.set_ylabel('x_1 (a.u.)')
+        self._ax_right.yaxis.set_label_position('right')
