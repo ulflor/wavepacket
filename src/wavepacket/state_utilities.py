#48 Fixed incorrect calculation of population.
The function was missing an abs(), which would fail for non-trivial
phases. The problem surfaced when I actually tried to use the function
in a real setting.

src/wavepacket/state_utilities.py
@@ -317,7 +317,7 @@ def population(state: wp.grid.State, target: wp.grid.State) -> float:
     target_trace = trace(target)
     if state.is_wave_function():
         coefficient = (np.conj(target.data) * state.data).sum()
-        return coefficient ** 2 / target_trace
+        return np.abs(coefficient) ** 2 / target_trace
     elif state.is_density_operator():
         matrix_form = np.reshape(state.data, [state.grid.size, state.grid.size])
         flat_target = np.ravel(target.data)

test/test_state_utilities.py
@@ -190,7 +190,7 @@ def test_population(grid_2d):
     states = wp.orthonormalize([wp.testing.random_state(grid_2d, seed) for seed in [1, 2]])
     a, target = (states[0], states[1])
 
-    psi = 2 * a + 0.7 * target
+    psi = 2 * a + 0.7 * target * np.sqrt(1j)   # create a complex scalar product to check for abs()
     rho = wp.builder.pure_density(psi)
     expected = 0.7 ** 2
 
@@ -205,7 +205,7 @@ def test_normalize_population_target(grid_2d):
     target = wp.testing.random_state(grid_2d, 1)
     target /= math.sqrt(wp.trace(target))
 
-    psi = 0.7 * target
+    psi = 0.7 * target * np.sqrt(1j)
     rho = wp.builder.pure_density(psi)
     expected = 0.7 ** 2
 